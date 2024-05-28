dce01a1a688343de8a4c99f7107964f114c4ad5b
Fishniki <nikichum96@gmail.com>
2024-05-28 08:13:16 +0700

    tes

--- controller/read_order.go
+++ controller/read_order.go
@@ -17,36 +17,52 @@ type StrukBelanja struct {
     MPembayaran string
 }
 
+type Pemesan struct {
+    Nama            string
+    MetodePembayaran string
+}
+
 func StrukPDF(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
     return func(w http.ResponseWriter, r *http.Request) {
+        err := r.ParseForm()
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusBadRequest)
+            return
+        }
 
-        r.ParseForm()
-        nameUser := r.Form["nama-user"][0]
-        jumlah := r.Form["jumlah"][0]
+        nameUser := r.FormValue("nama-user")
+        jumlah := r.FormValue("jumlah")
         MPembayaran := r.FormValue("metode-pembayaran")
 
-		jumlasstr, err := strconv.Atoi(jumlah)
-		if err != nil {
-			panic(err)
-		}
+        jumlasstr, err := strconv.Atoi(jumlah)
+        if err != nil {
+            http.Error(w, "Invalid jumlah", http.StatusBadRequest)
+            return
+        }
+
+        pemesanModel := []Pemesan{
+            {
+                Nama:            nameUser,
+                MetodePembayaran: MPembayaran,
+            },
+        }
 
         Struk, err := db.Query("SELECT id, nama, harga FROM `order` ORDER BY id DESC")
         if err != nil {
-            w.Write([]byte(err.Error()))
-            w.WriteHeader(http.StatusInternalServerError)
+            http.Error(w, err.Error(), http.StatusInternalServerError)
             return
         }
+        defer Struk.Close()
 
         StrukModel := []StrukBelanja{}
 
         for Struk.Next() {
+            var id, harga int
             var namabarang string
-            var harga, id int
 
             err = Struk.Scan(&id, &namabarang, &harga)
             if err != nil {
-                w.Write([]byte(err.Error()))
-                w.WriteHeader(http.StatusInternalServerError)
+                http.Error(w, err.Error(), http.StatusInternalServerError)
                 return
             }
 
@@ -64,23 +80,26 @@ func StrukPDF(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
             StrukModel = append(StrukModel, StrukList)
         }
 
+        if err = Struk.Err(); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
         fp := filepath.Join("view", "struk.html")
         tmpl, err := template.ParseFiles(fp)
         if err != nil {
-            w.Write([]byte(err.Error()))
-            w.WriteHeader(http.StatusInternalServerError)
+            http.Error(w, err.Error(), http.StatusInternalServerError)
             return
         }
 
         data := map[string]interface{}{
             "struk": StrukModel,
+            "user":  pemesanModel,
         }
 
         err = tmpl.Execute(w, data)
-
         if err != nil {
-            w.Write([]byte(err.Error()))
-            w.WriteHeader(http.StatusInternalServerError)
+            http.Error(w, err.Error(), http.StatusInternalServerError)
             return
         }
     }
